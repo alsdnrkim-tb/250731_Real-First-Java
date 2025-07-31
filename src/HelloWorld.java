c1491360f0aff94e46e87b0833280edd453f525c
mwk-mk0 <hhatp44@naver.com>
2025-07-31 11:07:32 +0900

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main(String[] text) {
+        System.out.println("Hello World!");
+        System.out.println("신 창 섭");
+        System.out.println("다 해줬잖아");
+    }
+}
